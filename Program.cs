a856186225a240d7f0520206c5d36046337e073f
Murtadha Jasim <ajasm672@gmail.com>
2025-06-07 13:10:23 +0300

    small

--- Program.cs
+++ Program.cs
@@ -99,13 +99,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     });
 
 // إعداد المنفذ من متغيرات البيئة (Railway يستخدم PORT)
-var port = Environment.GetEnvironmentVariable("PORT");
-if (string.IsNullOrEmpty(port))
-    port = "5000";
-builder.WebHost.ConfigureKestrel(options =>
-{
-    options.ListenAnyIP(int.Parse(port));
-});
+
 
 
 var app = builder.Build();
